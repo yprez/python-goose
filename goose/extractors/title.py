996571b553c0bf57dd32760ece106a3982464e8b
Yuri Prezument <y@yprez.com>
2015-02-19 19:09:40 +0200

    Improve index error checking in title_splitters

--- goose/extractors/title.py
+++ goose/extractors/title.py
@@ -51,19 +51,14 @@ class TitleExtractor(BaseExtractor):
         # my wonderfull article | TechCrunch
         title_words = title.split()
 
-        # check for an empty title
-        # so that we don't get an IndexError below
-        if len(title_words) == 0:
-            return u""
-
         # check if first letter is in TITLE_SPLITTERS
         # if so remove it
-        if title_words[0] in TITLE_SPLITTERS:
+        if title_words and title_words[0] in TITLE_SPLITTERS:
             title_words.pop(0)
 
         # check if last letter is in TITLE_SPLITTERS
         # if so remove it
-        if title_words[-1] in TITLE_SPLITTERS:
+        if title_words and title_words[-1] in TITLE_SPLITTERS:
             title_words.pop(-1)
 
         # rebuild the title
